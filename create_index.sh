0632731915b3e97981dc1b0c0df77c3fd13f7f74
rooooomania <rooooomania@icloud.com>
2016-11-25 19:51:17 +0900

    minor change in create_index.sh

--- create_index.sh
+++ create_index.sh
@@ -3,6 +3,6 @@ echo 'create index.md'
 
 URL="http://markdown-converter.s3-website-ap-northeast-1.amazonaws.com/artifacts"
 URL_ESCAPED=$(echo $URL |sed 's/\//\\\//g')
-find ./docs -type file |grep -e "^\./docs/" |grep -e '.*\.md$' | sed 's/^\./'${URL_ESCAPED}'/g' |sed 's/\.md/\.html/g' > ./docs/index.md
+find ./docs -type f |grep -e "^\./docs/" |grep -e '.*\.md$' | sed 's/^\./'${URL_ESCAPED}'/g' |sed 's/\.md/\.html/g' > ./docs/index.md
 
 echo 'index.md has been created'
