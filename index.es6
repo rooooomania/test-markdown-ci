bea7d0d0e1ac13c088c38209fb112d9b7f5231b7
rooooomania <rooooomania@icloud.com>
2016-11-29 18:45:25 +0900

    minor change

--- index.es6
+++ index.es6
@@ -1,3 +1,5 @@
 import md from './src/markdown-recursive';
-
 md();
+
+// TODO: 初回のCfn実行時に、`./src/*, create_index.sh, index.js`などを目的のgithubリポジトリにプッシュしてしまう。
+
--- index.js
+++ index.js
@@ -8,4 +8,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 (0, _markdownRecursive2.default)();
 
+// TODO: 初回のCfn実行時に、`./src/*, create_index.sh, index.js`などを目的のgithubリポジトリにプッシュしてしまう。
+
 //# sourceMappingURL=index.js.map
